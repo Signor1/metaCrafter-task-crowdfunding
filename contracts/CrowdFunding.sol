b8dcb6034af16985cd58329454055d06ed9d9c19
signor1 <emmanuelomemgboji@gmail.com>
2024-02-22 14:05:09 +0100

    feat: added contribute and checkgoalreached method

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -16,4 +16,22 @@ contract Crowdfunding {
         organizer = msg.sender;
         deadline = block.timestamp + (_durationInDays * 1 days);
     }
+
+    function contribute() external payable {
+        //checking if the deadline has reached or passed
+        require(
+            block.timestamp < deadline,
+            "The crowdfunding campaign has ended"
+        );
+        //ensuring that the amount is greater than 0
+        assert(msg.value > 0);
+
+        contributions[msg.sender] += msg.value;
+        totalFunds += msg.value;
+        contributors.push(msg.sender);
+    }
+
+    function checkGoalReached(uint fundingGoal) public view returns (bool) {
+        return totalFunds >= fundingGoal;
+    }
 }
