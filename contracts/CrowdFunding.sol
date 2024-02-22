8452071ead740abf709512ebf027864ba43305dc
signor1 <emmanuelomemgboji@gmail.com>
2024-02-22 14:04:04 +0100

    feat: added constructor

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -11,4 +11,9 @@ contract Crowdfunding {
     bool public campaignEnded;
 
     address[] public contributors;
+
+    constructor(uint _durationInDays) {
+        organizer = msg.sender;
+        deadline = block.timestamp + (_durationInDays * 1 days);
+    }
 }
