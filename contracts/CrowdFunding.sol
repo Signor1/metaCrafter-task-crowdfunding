10f79397878c72fcf698b937f16be870638f6ec3
signor1 <emmanuelomemgboji@gmail.com>
2024-02-22 14:12:18 +0100

    feat: added finalise campaign method

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -34,4 +34,41 @@ contract Crowdfunding {
     function checkGoalReached(uint fundingGoal) public view returns (bool) {
         return totalFunds >= fundingGoal;
     }
+
+    function finalizeCampaign() public {
+        //only the organiser can call this function
+        require(
+            msg.sender == organizer,
+            "Only the organizer can finalize the campaign"
+        );
+        //checking if the funding has ended
+        require(
+            block.timestamp >= deadline,
+            "The campaign deadline has not been reached yet"
+        );
+        //checking if the whole campaign for fund raising has ended
+        require(!campaignEnded, "The campaign has already been finalized");
+
+        if (checkGoalReached(100 ether)) {
+            if (payable(organizer).send(totalFunds)) {
+                campaignEnded = true;
+            } else {
+                revert("Failed to transfer funds to the organizer");
+            }
+        } else {
+            for (uint i = 0; i < contributors.length; i++) {
+                address payable contributor = payable(contributors[i]);
+                uint amount = contributions[contributor];
+                if (amount > 0) {
+                    if (contributor.send(amount)) {
+                        totalFunds -= amount;
+                        contributions[contributor] = 0;
+                    } else {
+                        revert("Failed to refund a contributor");
+                    }
+                }
+            }
+            campaignEnded = true;
+        }
+    }
 }
